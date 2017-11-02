f097b034a7b2573222e9afa5928b40fda70a2811
ckj96 <ckj96123@gmail.com>
2017-11-01 22:50:58 -0400

    add print string

---
+++ _build/parser.mli
@@ -0,0 +1,52 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | LBRACKET
+  | RBRACKET
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | ASSIGN
+  | NOT
+  | REMAINDER
+  | INNERPRODUCT
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | TRUE
+  | FALSE
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | FOR
+  | WHILE
+  | INT
+  | DOUBLE
+  | BOOL
+  | STRING
+  | ELIF
+  | BREAK
+  | CONTINUE
+  | VOID
+  | FUNCTION
+  | MATRIX
+  | IMAGE
+  | FILTER
+  | INT_LITERAL of (int)
+  | DOUBLE_LITERAL of (float)
+  | STRING_LITERAL of (string)
+  | ID of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
