81bef119e1009c72abe0a82811d05dedbdaa4597
glen <glenjoker@gmail.com>
2017-12-13 16:31:56 -0500

    1. fix the bug that matrix entries cannot be negative. 2. combine the code of stack and heap allocation of matrix.

---
+++ our_test/compiler_phase/matrix_8.ir.s
@@ -0,0 +1,245 @@
+	.text
+	.file	"MicroC"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# BB#0:                                 # %entry
+	pushq	%rbp
+.Lcfi0:
+	.cfi_def_cfa_offset 16
+.Lcfi1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Lcfi2:
+	.cfi_def_cfa_register %rbp
+	pushq	%r15
+	pushq	%r14
+	pushq	%r13
+	pushq	%r12
+	pushq	%rbx
+	subq	$72, %rsp
+.Lcfi3:
+	.cfi_offset %rbx, -56
+.Lcfi4:
+	.cfi_offset %r12, -48
+.Lcfi5:
+	.cfi_offset %r13, -40
+.Lcfi6:
+	.cfi_offset %r14, -32
+.Lcfi7:
+	.cfi_offset %r15, -24
+	movabsq	$-4615739258092021350, %rax # imm = 0xBFF199999999999A
+	movq	%rax, -112(%rbp)
+	movabsq	$-4611235658464650854, %rax # imm = 0xC00199999999999A
+	movq	%rax, -104(%rbp)
+	movabsq	$4614613358185178726, %rax # imm = 0x400A666666666666
+	movq	%rax, -96(%rbp)
+	movabsq	$-4606732058837280358, %rax # imm = 0xC01199999999999A
+	movq	%rax, -88(%rbp)
+	leaq	-112(%rbp), %rax
+	movq	%rax, -80(%rbp)
+	movabsq	$8589934594, %rdx       # imm = 0x200000002
+	movq	%rdx, -72(%rbp)
+	movl	$2, %eax
+	movq	%rsp, %rcx
+	addq	$-32, %rcx
+	movq	%rcx, %rsp
+	movq	%rcx, -64(%rbp)
+	movq	%rdx, -56(%rbp)
+	movl	$1, %edx
+	movl	$0, -44(%rbp)
+	cmpl	%edx, -44(%rbp)
+	jle	.LBB0_2
+	jmp	.LBB0_6
+	.p2align	4, 0x90
+.LBB0_5:                                # %merge
+                                        #   in Loop: Header=BB0_2 Depth=1
+	incl	-44(%rbp)
+	cmpl	%edx, -44(%rbp)
+	jg	.LBB0_6
+.LBB0_2:                                # %while_body
+                                        # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_4 Depth 2
+	movq	%rsp, %rdi
+	leaq	-16(%rdi), %rsi
+	movq	%rsi, %rsp
+	movl	$0, -16(%rdi)
+	cmpl	%edx, (%rsi)
+	jg	.LBB0_5
+	.p2align	4, 0x90
+.LBB0_4:                                # %while_body13
+                                        #   Parent Loop BB0_2 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	movl	-44(%rbp), %edi
+	imull	%eax, %edi
+	addl	(%rsi), %edi
+	movslq	%edi, %rdi
+	movq	$0, (%rcx,%rdi,8)
+	incl	(%rsi)
+	cmpl	%edx, (%rsi)
+	jle	.LBB0_4
+	jmp	.LBB0_5
+.LBB0_6:                                # %merge21
+	decl	%eax
+	movq	-64(%rbp), %r8
+	movl	-52(%rbp), %r10d
+	movq	-80(%rbp), %r11
+	movl	-68(%rbp), %r14d
+	movq	%rsp, %rcx
+	leaq	-16(%rcx), %rbx
+	movq	%rbx, %rsp
+	movl	$0, -16(%rcx)
+	cmpl	%eax, (%rbx)
+	jle	.LBB0_8
+	jmp	.LBB0_12
+	.p2align	4, 0x90
+.LBB0_11:                               # %merge56
+                                        #   in Loop: Header=BB0_8 Depth=1
+	incl	(%rbx)
+	cmpl	%eax, (%rbx)
+	jg	.LBB0_12
+.LBB0_8:                                # %while_body36
+                                        # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_10 Depth 2
+	movq	%rsp, %r9
+	leaq	-16(%r9), %rcx
+	movq	%rcx, %rsp
+	movl	$0, -16(%r9)
+	cmpl	%eax, (%rcx)
+	jg	.LBB0_11
+	.p2align	4, 0x90
+.LBB0_10:                               # %while_body39
+                                        #   Parent Loop BB0_8 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	movl	(%rcx), %edx
+	movl	(%rbx), %esi
+	movl	%r10d, %edi
+	imull	%esi, %edi
+	addl	%edx, %edi
+	movslq	%edi, %rdi
+	imull	%r14d, %esi
+	addl	%edx, %esi
+	movslq	%esi, %rdx
+	movsd	(%r11,%rdx,8), %xmm0    # xmm0 = mem[0],zero
+	movsd	%xmm0, (%r8,%rdi,8)
+	incl	(%rcx)
+	cmpl	%eax, (%rcx)
+	jle	.LBB0_10
+	jmp	.LBB0_11
+.LBB0_12:                               # %merge61
+	movq	-64(%rbp), %r15
+	movl	-56(%rbp), %eax
+	movl	-52(%rbp), %r12d
+	decl	%eax
+	movl	%eax, -48(%rbp)         # 4-byte Spill
+	leal	-1(%r12), %r13d
+	movq	%rsp, %rax
+	leaq	-16(%rax), %rbx
+	movq	%rbx, %rsp
+	movl	$0, -16(%rax)
+	jmp	.LBB0_13
+	.p2align	4, 0x90
+.LBB0_17:                               # %merge87
+                                        #   in Loop: Header=BB0_13 Depth=1
+	movl	$.Lfmt_str, %edi
+	movl	$.Lfmt_str.1, %esi
+	xorl	%eax, %eax
+	callq	printf
+	incl	(%rbx)
+.LBB0_13:                               # %while73
+                                        # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_16 Depth 2
+	movl	-48(%rbp), %eax         # 4-byte Reload
+	cmpl	%eax, (%rbx)
+	jg	.LBB0_18
+# BB#14:                                # %while_body74
+                                        #   in Loop: Header=BB0_13 Depth=1
+	movq	%rsp, %rax
+	leaq	-16(%rax), %r14
+	movq	%r14, %rsp
+	movl	$0, -16(%rax)
+	cmpl	%r13d, (%r14)
+	jg	.LBB0_17
+	.p2align	4, 0x90
+.LBB0_16:                               # %while_body77
+                                        #   Parent Loop BB0_13 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	movl	(%rbx), %eax
+	imull	%r12d, %eax
+	addl	(%r14), %eax
+	cltq
+	movsd	(%r15,%rax,8), %xmm0    # xmm0 = mem[0],zero
+	movl	$.Lfmt_double, %edi
+	movb	$1, %al
+	callq	printf
+	movl	$.Lfmt_str, %edi
+	movl	$.Lfmt_str.2, %esi
+	xorl	%eax, %eax
+	callq	printf
+	incl	(%r14)
+	cmpl	%r13d, (%r14)
+	jle	.LBB0_16
+	jmp	.LBB0_17
+.LBB0_18:                               # %merge93
+	movl	$.Lfmt_str, %edi
+	movl	$.Lfmt_str.3, %esi
+	xorl	%eax, %eax
+	callq	printf
+	xorl	%eax, %eax
+	leaq	-40(%rbp), %rsp
+	popq	%rbx
+	popq	%r12
+	popq	%r13
+	popq	%r14
+	popq	%r15
+	popq	%rbp
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.Lfmt_str,@object       # @fmt_str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lfmt_str:
+	.asciz	"%s"
+	.size	.Lfmt_str, 3
+
+	.type	.Lfmt_double,@object    # @fmt_double
+.Lfmt_double:
+	.asciz	"%f"
+	.size	.Lfmt_double, 3
+
+	.type	.Lfmt_int,@object       # @fmt_int
+.Lfmt_int:
+	.asciz	"%d"
+	.size	.Lfmt_int, 3
+
+	.type	.Lfmt_str.1,@object     # @fmt_str.1
+.Lfmt_str.1:
+	.asciz	"\n"
+	.size	.Lfmt_str.1, 2
+
+	.type	.Lfmt_str.2,@object     # @fmt_str.2
+.Lfmt_str.2:
+	.asciz	"  "
+	.size	.Lfmt_str.2, 3
+
+	.type	.Lfmt_str.3,@object     # @fmt_str.3
+.Lfmt_str.3:
+	.zero	1
+	.size	.Lfmt_str.3, 1
+
+	.type	.Lfmt_str.4,@object     # @fmt_str.4
+.Lfmt_str.4:
+	.asciz	"true"
+	.size	.Lfmt_str.4, 5
+
+	.type	.Lfmt_str.5,@object     # @fmt_str.5
+.Lfmt_str.5:
+	.asciz	"false"
+	.size	.Lfmt_str.5, 6
+
+
+	.section	".note.GNU-stack","",@progbits
