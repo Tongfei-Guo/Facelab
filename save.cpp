af6e77680a7c7ecd47e697509e5287aa1fbb7b5b
weiman <ws2517@columbia.edu>
2017-12-16 20:24:35 -0500

    built-in function load&save

---
+++ save.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace cv;
+
+using namespace std;
+
+void save(double* input, char* fileName) 
+{
+    int width = input[0];
+    int height = input[1];
+    double* data = new double[3*width*height];
+    for(int i = 0; i < 3*width*height; i++) data[i]=input[i+2];
+    Mat image = cv::Mat(height, width, CV_64FC3, data);
+    imwrite(fileName,image);
+    return;
+}
