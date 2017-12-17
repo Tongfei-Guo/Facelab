2389f68fe89f577db418ed73f24fadc238fbeb73
weiman <ws2517@columbia.edu>
2017-12-16 20:50:52 -0500

    fix argument

--- load.cpp
+++ load.cpp
@@ -17,7 +17,7 @@
 using namespace cv;
 
 using namespace std;
-double* load(String imageName)
+double* load(char imageName[])
 {
     Mat img = imread(imageName,CV_LOAD_IMAGE_COLOR);
     unsigned char* input = (unsigned char*)(img.data);
